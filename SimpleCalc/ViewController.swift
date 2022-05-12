88b67ed626c577aacae57389980ed674e4c403a1
aitkazindev <aitkazindev@gmail.com>
2022-05-12 14:48:21 +0900

    Label changing after pressing button

--- SimpleCalc/ViewController.swift
+++ SimpleCalc/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func touched(_ sender: Any) {
+        label.text = "Hello User"
+    }
 
 }
 
