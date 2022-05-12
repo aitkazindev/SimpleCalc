65553c93d21813f6aad8f54e41625831a3c5a799
aitkazindev <aitkazindev@gmail.com>
2022-05-12 16:21:42 +0900

    completed basic functions

--- SimpleCalc/ViewController.swift
+++ SimpleCalc/ViewController.swift
@@ -12,18 +12,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textField2: UITextField!
-    
+    var a = 0;
+    var b = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     @IBAction func touched(_ sender: Any) {
-        let a = textField.text!
-        let b = textField2.text!
-        
-        let sum = Int(a)! + Int(b)!
+        a = Int(textField.text!) ?? 0
+        b = Int(textField2.text!) ?? 0
+        let sum = a + b
         label.text = "sum = \(sum) "
+    }
+    @IBAction func minus(_ sender: Any) {
+        a = Int(textField.text!) ?? 0
+        b = Int(textField2.text!) ?? 0
+        let minus = a - b
+        label.text = "minus = \(minus) "
+    }
+    
+    @IBAction func dev(_ sender: Any) {
+        a = Int(textField.text!) ?? 0
+        b = Int(textField2.text!) ?? 0
+        if b == 0 {
+            label.text = "error(impossible to devide on 0"
+        }else{
+            let dev = a / b
+            label.text = "dev = \(dev) "
+        }
+       
+        
+    }
+    
+    @IBAction func mult(_ sender: Any) {
+        a = Int(textField.text!) ?? 0
+        b = Int(textField2.text!) ?? 0
+        let mult = a * b
+        label.text = "mult = \(mult) "
     }
 }
 
